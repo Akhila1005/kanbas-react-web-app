cff5444bc94eb1687584d7a27a542230d7e45703
akhila1005 <chiguluri.a@northeastern.edu>
2024-04-02 11:58:05 -0400

    labs

--- src/Labs/a5/index.tsx
+++ src/Labs/a5/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import EncodingParametersInURLs from "./EncodingParametersInURLs";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithObjects from "./WorkingWithObjects";
+
 function Assignment5() {
+  const API_BASE = process.env.REACT_APP_API_BASE;
     return (
       <div>
         <h1>Assignment 5</h1>
-        <a href="http://localhost:4000/a5/welcome">
+        <a href={API_BASE + "/a5/welcome"}>
           Welcome
         </a>
+        <EncodingParametersInURLs/>
+        <WorkingWithObjects/>
+        <WorkingWithArrays/>
       </div>
     );
   }
-  export default Assignment5;
-  
-  
+  export default Assignment5
